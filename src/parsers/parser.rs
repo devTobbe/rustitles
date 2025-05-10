4929bda6739f6b8a0ae0df38f7e6b1671460b914
devTobbe <32337476+devTobbe@users.noreply.github.com>
2025-05-10 16:25:06 +0200

    feat: add SubtitleParser trait

--- src/parsers/parser.rs
+++ src/parsers/parser.rs
@@ -1,3 +1,7 @@
 
 const TAG_PATTERN : String = r"<[^>]+>";
 const BRACKET_PATTERN : String = r"{[^}]+}";
+
+pub trait SubtitleParser {
+    fn parse(&self, input: String) -> Result<Vec<Subtitle>, Error>
+}
