dbc5f8cd49ef46b29555afde7a58a65a38ae89ef
devTobbe <32337476+devTobbe@users.noreply.github.com>
2025-05-11 14:17:12 +0200

    feat: implement srt parser

--- src/parsers/srt.rs
+++ src/parsers/srt.rs
@@ -1,2 +1,22 @@
+use std::io::Error;
+use regex::Regex;
 
-const SRT_PATTERN: String = r"(?m)^\d+?\n(\d{2}:\d{2}:\d{2}[.,]\d{3})\s+-->\s+(\d{2}:\d{2}:\d{2}[.,]\d{3})(?:[^\n])*\n((?:[^\n]+\n?)+)";
+use crate::parsers::parser::SubtitleParser;
+use crate::parsers::types::Caption;
+use crate::parsers::types::Subtitle;
+
+const SRT_PATTERN: &str = r"(?m)^\d+?\n(\d{2}:\d{2}:\d{2}[.,]\d{3})\s+-->\s+(\d{2}:\d{2}:\d{2}[.,]\d{3})(?:[^\n])*\n((?:[^\n]+\n?)+)";
+
+pub struct SrtParser;
+
+impl SubtitleParser for SrtParser {
+    fn parse(&self, input: &str) -> Result<Subtitle, Error> {
+        let mut subs = Subtitle::new();
+        let re = Regex::new(SRT_PATTERN).unwrap();
+        for caps in re.captures_iter(input){
+            let caption = Caption::new(caps[1].to_string(), caps[2].to_string(), caps[3].to_string());
+            subs.captions.push(caption);
+        };
+        Ok(subs)
+    }
+}
