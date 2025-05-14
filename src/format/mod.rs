60383be8bcc9d3138822fe4e3002179dcfa84493
devTobbe <32337476+devTobbe@users.noreply.github.com>
2025-05-14 20:17:26 +0200

    chore: hide unused regexes for later

--- src/format/mod.rs
+++ src/format/mod.rs
@@ -7,8 +7,9 @@ pub mod model;
 use model::Subtitle;
 use std::io::Error;
 
-const TAG_PATTERN: &str = r"<[^>]+>";
-const BRACKET_PATTERN: &str = r"{[^}]+}";
+// TODO: Add these to clean subtitles later on... Maybe?
+//const TAG_PATTERN: &str = r"<[^>]+>";
+//const BRACKET_PATTERN: &str = r"{[^}]+}";
 
 pub trait SubtitleParser {
     fn parse(&self, input: &str) -> Result<Subtitle, Error>;
