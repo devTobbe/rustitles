c6bde30a3f86133479f2a98073f1734e1d262b87
devTobbe <32337476+devTobbe@users.noreply.github.com>
2025-05-15 21:32:12 +0200

    feat: test sniff functionality

---
+++ tests/sniff_tests.rs
@@ -0,0 +1,67 @@
+const TESTSRT: &str = r"1
+00:00:01,000 --> 00:00:04,000
+Hello, world!
+";
+
+const TESTASS: &str = r"[Script Info]
+Title: Example ASS Subtitle
+Original Script: Your Name
+ScriptType: v4.00+
+Collisions: Normal
+PlayDepth: 0
+Timer: 100.0000
+
+[V4+ Styles]
+Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
+Style: Default,Arial,24,&H00FFFFFF,&H000000FF,&H00000000,&H64000000,-1,0,0,0,100,100,0,0,1,1,0,2,10,10,10,1
+Style: Highlight,Arial,24,&H00FFFF00,&H000000FF,&H00000000,&H64000000,-1,0,0,0,100,100,0,0,1,1,0,2,10,10,10,1
+
+[Events]
+Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
+Dialogue: 0,0:00:01.00,0:00:05.00,Default,,0,0,0,,{\i1}Hello, this is a test subtitle in ASS. I am looking forward to writing this parser...
+Dialogue: 0,0:00:06.00,0:00:10.00,Default,,0,0,0,,{\b1}Another line of text here with bold.
+Dialogue: 0,0:00:12.00,0:00:15.00,Highlight,,0,0,0,,{\c&HFF0000&}This is the final test line in red.
+";
+
+const TESTVTT: &str = r"WEBVTT
+
+1
+00:00:01,000 --> 00:00:04,000
+Hello, world!
+";
+
+#[cfg(test)]
+mod tests {
+    use rustitles::{format::model::SubFormat, format::sniff::sniff_format};
+
+    use super::*;
+
+    #[test]
+    pub fn test_sniff_srt() {
+        let result = sniff_format(&TESTSRT);
+
+        assert_eq!(result.unwrap(), SubFormat::SRT);
+   }
+
+    #[test]
+    pub fn test_sniff_ass() {
+        let result = sniff_format(&TESTASS);
+
+        assert_eq!(result.unwrap(), SubFormat::ASS);
+    }
+
+    #[test]
+    pub fn test_sniff_vtt() {
+        let result = sniff_format(&TESTVTT);
+
+        assert_eq!(result.unwrap(), SubFormat::VTT);
+    }
+
+    #[test]
+    pub fn test_sniff_err() {
+        let input = r"THIS SHOULD FAIL";
+        let result = sniff_format(input);
+
+        assert_eq!(result, None)
+    }
+}
