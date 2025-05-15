1eebf4287d8e5feda87641df3403d2db4c307290
devTobbe <32337476+devTobbe@users.noreply.github.com>
2025-05-15 20:51:18 +0200

    docs: add model comments

--- src/format/model.rs
+++ src/format/model.rs
@@ -1,9 +1,11 @@
+// Supported Subtitle formats
 pub enum SubFormat {
     SRT,
     ASS,
     VTT,
 }
 
+// Structure for a single caption
 #[derive(Debug, PartialEq)]
 pub struct Caption {
     pub start: String,
@@ -11,6 +13,7 @@ pub struct Caption {
     pub text: String,
 }
 
+// Structure for a subttile, which contains a vector of Captions
 #[derive(Debug, PartialEq)]
 pub struct Subtitle {
     pub captions: Vec<Caption>,
