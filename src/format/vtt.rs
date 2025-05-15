163bdccd00fc25743ebacac4d51d16f797744710
devTobbe <32337476+devTobbe@users.noreply.github.com>
2025-05-15 21:31:33 +0200

    fix: adjust VTT regex to work properly

--- src/format/vtt.rs
+++ src/format/vtt.rs
@@ -5,7 +5,7 @@ use crate::format::SubtitleParser;
 use crate::format::model::Caption;
 use crate::format::model::Subtitle;
 
-const VTT_PATTERN: &str = r"(?m)^(?:WEBVTT\n)?(?:\d+\n)?(\d{2}:\d{2}:\d{2}\.\d{3})\s*-->\s*(\d{2}:\d{2}:\d{2}\.\d{3})\n((?:[^\n]+\n?)*)\n";
+const VTT_PATTERN: &str = r"(?m)^(?:WEBVTT\s*\n{1,2})?^\d+\n(\d{2}:\d{2}:\d{2}[.,]\d{3})\s*-->\s*(\d{2}:\d{2}:\d{2}[.,]\d{3})(?:[^\n]*)\n((?:[^\n]+\n?)*)";
 
 pub struct VttParser;
 
