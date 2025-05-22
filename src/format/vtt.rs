533a89ef3674e33ac20f560be4dbfe9a38dcafc8
devTobbe <32337476+devTobbe@users.noreply.github.com>
2025-05-22 21:10:36 +0200

    refactor: vtt parser functional rewrite

--- src/format/vtt.rs
+++ src/format/vtt.rs
@@ -21,14 +21,19 @@ impl SubtitleParser for VttParser {
                 panic!("{error}");
             }
         };
-        for caps in re.captures_iter(input) {
-            let caption = Caption::new(
-                caps[1].to_string(),
-                caps[2].to_string(),
-                caps[3].trim().to_string(),
-            );
-            subs.captions.push(caption);
-        }
-        Ok(subs)
+
+        let captions = re
+            .captures_iter(input)
+            .map(|caps| {
+                Caption::new(
+                    caps[1].to_string(),
+                    caps[2].to_string(),
+                    caps[3].trim().to_string(),
+                )
+            })
+            .collect();
+
+        Ok(Subtitle { captions })
+
     }
 }
